447fe9901a59b64f798e00dbae1a4b2d4d1dded5
Timur Latush <timguy4894@gmail.com>
2024-02-15 19:28:07 +0100

    fixed some issues with user token

--- screens/WelcomeScreen.js
+++ screens/WelcomeScreen.js
@@ -1,9 +1,23 @@
 import { StyleSheet, Text, View } from 'react-native';
+import { AuthContext } from '../store/auth-context'; 
+import { useContext } from 'react';
+import axios from 'axios';
+import { useEffect, useState } from 'react';
 
 function WelcomeScreen() {
+  const [fechedMessage, setFechedMessage] = useState('')
+  const authCtx = useContext(AuthContext)
+  const token = authCtx.token
+
+  useEffect(()=>{
+    axios.get('https://react-native-course-45a99-default-rtdb.firebaseio.com/message.json?auth='+token).then(
+      (response)=>{setFechedMessage(response.data)}
+    )
+  }, [token])
+
   return (
     <View style={styles.rootContainer}>
-      <Text style={styles.title}>Welcome!</Text>
+      <Text style={styles.title}>{fechedMessage}</Text>
       <Text>You authenticated successfully!</Text>
     </View>
   );
