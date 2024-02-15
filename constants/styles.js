8040d738798328ffbb2859f1a1fe5187ae672379
Timur Latush <timguy4894@gmail.com>
2024-02-15 18:32:06 +0100

    first commit

---
+++ constants/styles.js
@@ -0,0 +1,7 @@
+export const Colors = {
+  primary100: '#f5f5f5',
+  primary500: '#cccccc',
+  primary800: '#333333',
+  error100: '#f5f5f5',
+  error500: '#cccccc',
+}
---
+++ screens/SignupScreen.js
@@ -0,0 +1,32 @@
+import { useContext, useState } from 'react';
+import { Alert } from 'react-native';
+import AuthContent from '../components/auth/AuthContent';
+import LoadingOverlay from '../components/ui/LoadingOverlay';
+import { createUser } from '../util/auth';
+import { AuthContext } from '../store/auth-context';
+
+function SignupScreen() {
+  const [isAuthenticating, setIsAuthenticating] = useState(false);
+
+    const authCtx = useContext(AuthContext)
+
+  async function signupHandler({ email, password }) {
+    setIsAuthenticating(true);
+    try {
+        const token = await createUser(email, password);
+        authCtx.authenticate(token)
+    } catch (error) {
+        Alert.alert("Authentification failed", "Could not log you in. Please, check your credentials")
+        setIsAuthenticating(false);
+    }
+    
+  }
+
+  if (isAuthenticating) {
+    return <LoadingOverlay message="Creating user..." />;
+  }
+
+  return <AuthContent onAuthenticate={signupHandler} />;
+}
+
+export default SignupScreen;
